b7e8a1a59fe22a5841ec55a4df2b7bacf60c55a5
Maciej Baczmanski <maciej.baczmanski@nordicsemi.no>
2025-02-28 09:05:24 +0100

    WIP modify zap_generate
    Signed-off-by: Maciej Baczmanski <maciej.baczmanski@nordicsemi.no>

--- scripts/west/zap_generate.py
+++ scripts/west/zap_generate.py
@@ -3,10 +3,14 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
 import argparse
+import tempfile
+import shutil
+import re
 import os
 import sys
 
 from textwrap import dedent
+from pathlib import Path
 
 from west import log
 from west.commands import CommandError, WestCommand
@@ -36,6 +40,7 @@ class ZapGenerate(WestCommand):
                             help='Path where to store the generated files')
         parser.add_argument('-m', '--matter-path', type=existing_dir_path,
                             default=DEFAULT_MATTER_PATH, help='Path to Matter SDK')
+        parser.add_argument('-f', '--full', action='store_true', help='Generate full data model files')
         return parser
 
     def do_run(self, args, unknown_args):
@@ -63,9 +68,30 @@ class ZapGenerate(WestCommand):
 
         cmd = [sys.executable, zap_generate_path]
         cmd += [zap_file_path]
-        cmd += ["-t", app_templates_path]
-        cmd += ["-o", output_path]
 
-        self.check_call([str(x) for x in cmd])
+
+        self.check_call([str(x) for x in cmd + ["-o", output_path] + ["-t", app_templates_path]])
+        if args.full:
+            with tempfile.TemporaryDirectory() as tmpdir:
+                tmpdir = Path(tmpdir)
+                shutil.copytree(args.matter_path / "src/app/common/templates", tmpdir / "templates", dirs_exist_ok=True)
+                shutil.copytree(args.matter_path / "src/app/zap-templates", tmpdir / "zap-templates", dirs_exist_ok=True)
+                templates_json_path = tmpdir / "templates/templates.json"
+                with open(templates_json_path, 'r+') as file:
+                    content = file.read()
+                    content = content.replace("../../zap-templates", "../zap-templates")
+                    file.seek(0)
+                    file.write(content)
+                    file.truncate()
+
+                # for zapt_file in (tmpdir / "zap-templates").rglob("*.zapt"):
+                #     with open(zapt_file, 'r+') as file:
+                #         content = file.read()
+                #         content = re.sub(r'#include <app-common/zap-generated/(.*?)>', r'#include "zap-generated/\1"', content, flags=re.MULTILINE)
+                #         file.seek(0)
+                #         file.write(content)
+                #         file.truncate()
+                self.check_call([str(x) for x in cmd + ["-o", output_path / "app-common/zap-generated"] + ["-t", tmpdir / "templates/templates.json"]])
+        # else:
 
         log.inf(f"Done. Files generated in {output_path}")
